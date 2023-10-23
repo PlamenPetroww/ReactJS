160b6f11f6ab206beaa9d44236669fce3c96add0
plamenpetroww <plamenpetroww@gmail.com>
2023-10-23 14:56:51 +0200

    create-react-app

---
+++ 01.Intro to React and JSX/first-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Link from './components/Link';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Link />
+      </header>
+    </div>
+  );
+}
+
+export default App;
