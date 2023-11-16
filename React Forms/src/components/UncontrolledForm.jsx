2e18384d21c0684903845df33efdce6bf45fa0ea
plamenpetroww <plamenpetroww@gmail.com>
2023-11-16 06:58:04 +0100

    create div with Username and Password

---
+++ React Forms/src/components/UncontrolledForm.jsx
@@ -0,0 +1,21 @@
+export default function UncontrolledForm() {
+    return (
+        <>
+            <h1>Uncontrolled Form</h1>
+
+            <form>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password"/>
+                </div>
+                <div>
+                    <input type="submit" value="Register" />
+                </div>
+            </form>
+        </>
+    );
+};
