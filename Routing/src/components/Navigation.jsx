de2d50627d2b8dcbbc9ce2374643176a64c3dddc
plamenpetroww <plamenpetroww@gmail.com>
2023-11-20 04:27:13 +0100

    make  contacts component, about, component and gallery component

--- Routing/src/components/Navigation.jsx
+++ Routing/src/components/Navigation.jsx
@@ -13,7 +13,7 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} style={({isActive}) => isActive ? {backgroundColor: 'red'} : {}} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
             <Nav.Link as={NavLink} style={({isActive}) => isActive ? {backgroundColor: 'red'} : {}} to="/about">About</Nav.Link>
             <Nav.Link as={NavLink} style={({isActive}) => isActive ? {backgroundColor: 'red'} : {}} to="/contacts">Contacts</Nav.Link>
             <Nav.Link as={NavLink} style={({isActive}) => isActive ? {backgroundColor: 'red'} : {}} to="/characters">Characters</Nav.Link>
